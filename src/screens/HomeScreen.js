a3840a2c96109c97fa9592b64863ab926a14b69c
Appboxer Dev <sandeepa.singh@startxlabs.com>
2020-05-08 16:57:32 +0530

    uodate

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,15 +1,32 @@
-import React from "react";
-import { Text, StyleSheet,TouchableOpacity} from "react-native";
+import React,{Component} from "react";
+import { Text, View,StyleSheet,TouchableOpacity,TextInput} from "react-native";
+import {Button} from 'native-base';
 
-
-const HomeScreen = () => {
-  return <TouchableOpacity onPress={()=>console.log('hey sandeepa')}><Text style={styles.text}>Hey Abhinav Welcome to my app</Text></TouchableOpacity>;
+class HomeScreen extends Component{
+// const HomeScreen = () => {
+  onSubmit=()=>{
+    console.log('called');
+  }
+  render(){
+  return(
+     <View><Text style={styles.text}>Hey! wanna see some cool images from an outside world?</Text>
+  <Text>Just login </Text>
+  <TextInput
+  placeholder="Enter Asteroid ID"
+  />
+  <Button><Text>Random Asteroid</Text></Button>
+  <Button onPress={()=>this.onSubmit()}>Submit</Button>
+  </View>
+  );
+  }
 };
 
 const styles = StyleSheet.create({
   text: {
-    fontSize: 30,
-    color:'green',
+    fontSize: 18,
+    color:'blue',
+    textAlign:'center',
+    marginHorizontal:30,
   }
 });
 
